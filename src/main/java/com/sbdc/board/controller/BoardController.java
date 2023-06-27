0af88d68cba0d202fab867b0357c0ee151cb1c69
sbdcproject <137793348+sbdcproject@users.noreply.github.com>
2023-06-27 23:54:07 +0900

    6/27 commit

---
+++ src/main/java/com/sbdc/board/controller/BoardController.java
@@ -0,0 +1,27 @@
+package com.sbdc.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BoardController {
+    @GetMapping("/mainframe")
+    public String mainframeportal(){
+
+        return "MainFrame";
+    }
+
+    @GetMapping("/subframe1")
+    public String subframeportal1(){
+
+        return "SubFrame1";
+    }
+
+    @GetMapping("/subframe2")
+    public String subframeportal2(){
+
+        return "SubFrame2";
+    }
+
+}
