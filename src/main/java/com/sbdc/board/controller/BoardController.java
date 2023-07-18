0af0fc5b233bdd3fcb73882ef53bbe7b7ab85e13
Welcome <gksrnr2675@naver.com>
2023-07-18 17:08:57 +0900

    7/18 17:00 commit

--- src/main/java/com/sbdc/board/controller/BoardController.java
+++ src/main/java/com/sbdc/board/controller/BoardController.java
@@ -56,7 +56,8 @@ public class BoardController {
 
         int nowPage = list.getPageable().getPageNumber() + 1;
         int startPage = Math.max(nowPage - 4, 1);
-        int endPage = Math.min(nowPage + 5, list.getTotalPages());
+        int endPage = (list.getTotalPages() == 0) ? 1 : Math.min(nowPage + 5, list.getTotalPages());
+
 
         model.addAttribute("list", list);
         model.addAttribute("nowPage", nowPage);
