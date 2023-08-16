8e2dc833d7b2473f0c1e0b3872f71eee533b1a94
yeojin <wnduwls0603@naver.com>
2023-08-16 14:24:59 +0900

    08/16 14:30 commit

--- src/main/java/com/sbdc/board/controller/BoardController.java
+++ src/main/java/com/sbdc/board/controller/BoardController.java
@@ -2,20 +2,27 @@ package com.sbdc.board.controller;
 
 import com.sbdc.board.entity.TB_BOARD;
 
+import com.sbdc.board.entity.TB_BOARD_SAVE;
 import com.sbdc.board.service.BoardService;
+import com.sun.istack.NotNull;
+import com.sun.org.apache.xpath.internal.objects.XNumber;
+import com.sun.org.apache.xpath.internal.operations.Number;
+import org.springframework.boot.env.SystemEnvironmentPropertySourceEnvironmentPostProcessor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import sun.awt.geom.AreaOp;
+import sun.util.calendar.LocalGregorianCalendar;
 
-import java.util.List;
+import javax.xml.soap.SOAPPart;
+import java.sql.SQLOutput;
+import java.util.Date;
 
 
 @Controller
@@ -34,29 +41,33 @@ public class BoardController {
         return "SubFrame1";
     }
 
+
     @GetMapping("/subframe2")
     public String subframeportal2(){
 
         return "SubFrame2";
     }
 
+
+
+    @Nullable
     @GetMapping("/mainboard")
     public String boardList(Model model,
                             @PageableDefault(page = 0, size = 10) Pageable pageable,
                             String searchKeyword, String searchKeyword1, String searchKeyword2){
 
         Page<TB_BOARD> list = null;
-        Page<TB_BOARD> certificationlist = null;
 
         if(searchKeyword == null && searchKeyword1 == null && searchKeyword2 == null) {
             list = boardService.boardList(pageable);
         }else {
-            list = boardService.boardSearchList(searchKeyword,searchKeyword1, searchKeyword2, pageable);
+            list = boardService.boardSearchList(searchKeyword, searchKeyword1, searchKeyword2, pageable);
         }
 
         int nowPage = list.getPageable().getPageNumber() + 1;
         int startPage = Math.max(nowPage - 4, 1);
-        int endPage = Math.min(nowPage + 5, list.getTotalPages());
+        int endPage = (list.getTotalPages() == 0) ? 1 : Math.min(nowPage + 5, list.getTotalPages());
+
 
         model.addAttribute("list", list);
         model.addAttribute("nowPage", nowPage);
@@ -66,8 +77,41 @@ public class BoardController {
         return "MainBoard";
     }
 
+
+    @Nullable
+    @PostMapping("/mainboard/save")
+    public String boardSavePro(TB_BOARD_SAVE boardsave) {
+
+        System.out.println(boardsave.getCOMPANYNAME_SAVE());
+        System.out.println(boardsave.getCOLUMN1_SAVE());
+        System.out.println(boardsave.getCERTIFICATION_SAVE());
+        System.out.println(boardsave.getSERVICECODE_SAVE());
+        System.out.println(boardsave.getCOMPANYNUM_SAVE());
+        System.out.println(boardsave.getClass());
+
+
+        boardService.boardsave(boardsave);
+
+
+        return "redirect:/mainboard/";
+    }
+
+
     @GetMapping("/myboard")
-    public String myboard(){
+    public String myboard(Model model,
+                          @PageableDefault(page = 0, size = 10) Pageable pageable){
+
+        Page<TB_BOARD_SAVE> list = boardService.boardsaveList(pageable);
+
+        int nowPage = list.getPageable().getPageNumber() + 1;
+        int startPage = Math.max(nowPage - 4, 1);
+        int endPage = (list.getTotalPages() == 0) ? 1 : Math.min(nowPage + 5, list.getTotalPages());
+
+
+        model.addAttribute("list", list);
+        model.addAttribute("nowPage", nowPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
 
         return "MyBoard";
     }
@@ -78,4 +122,11 @@ public class BoardController {
         return "SubFrame3";
     }
 
+    @PostMapping("/myboard/delete")
+    public String boardDelete(String COMPANYNUM_SAVE){
+        System.out.println(COMPANYNUM_SAVE);
+        boardService.boardDelete(COMPANYNUM_SAVE);
+
+        return "redirect:/myboard";
+    }
 }
--- src/main/java/com/sbdc/board/repository/BoardRepository_save.java
+++ src/main/java/com/sbdc/board/repository/BoardRepository_save.java
@@ -1,17 +1,15 @@
 package com.sbdc.board.repository;
 
-import com.sbdc.board.entity.TB_BOARD;
 import com.sbdc.board.entity.TB_BOARD_SAVE;
+import org.hibernate.mapping.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BoardRepository extends JpaRepository<TB_BOARD, Integer> {
-    @Nullable
-    Page<TB_BOARD> findByCERTIFICATIONContainingAndSERVICECODEContainingAndCOMPANYNUMContaining(String searchKeyword, String searchKeyword1, String searchKeyword2, Pageable pageable);
-
+public interface BoardRepository_save extends JpaRepository<TB_BOARD_SAVE, String> {
 
 }
