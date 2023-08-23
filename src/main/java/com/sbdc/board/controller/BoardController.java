f594b95d01579281eac2d3e9ea6fe81b01012f87
Welcome <gksrnr2675@naver.com>
2023-08-23 18:05:26 +0900

    8/18 18:00 commit

--- src/main/java/com/sbdc/board/controller/BoardController.java
+++ src/main/java/com/sbdc/board/controller/BoardController.java
@@ -7,6 +7,7 @@ import com.sbdc.board.entity.TB_BOARD_SAVE;
 import com.sbdc.board.service.BoardService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
@@ -57,11 +58,13 @@ public class BoardController {
         int nowPage = list.getPageable().getPageNumber() + 1;
         int startPage = Math.max(nowPage - 4, 1);
         int endPage = (list.getTotalPages() == 0) ? 1 : Math.min(nowPage + 4, list.getTotalPages());
+        int totalPage = list.getTotalPages();
 
         model.addAttribute("list", list);
         model.addAttribute("nowPage", nowPage);
         model.addAttribute("startPage", startPage);
         model.addAttribute("endPage", endPage);
+        model.addAttribute("totalPage", totalPage);
 
         return "MainBoard";
     }
